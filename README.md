3bd3f9c33b99052e271ca2eebb1822107e150c5d
Rickymtl <53066199+Rickymtl@users.noreply.github.com>
2024-05-22 01:06:30 -0400

    proj init

---
+++ README.md
@@ -0,0 +1,3 @@
+# GCP website of useful tools
+In this project we create a website of useful little tools
+for GCP deployment. 
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+common_requires = [
+    'rembg',
+    'flask',
+    'pillow'
+]
+
+setup(
+    name='gcp-website',
+    version='0.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='mao',
+    author_email='tianlemao98@gmail.com',
+    description='',
+    install_requires=common_requires,
+)
